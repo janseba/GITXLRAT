5536ff2c2daf5d6899092942daab3b6a12ae281b
Bas Jansen <bas.jansen@infoaction.nl>
2017-03-22 17:22:52 +0100

    Changed to level of AlternativeSKU

--- Views/View_VolumeDistributionKeys.sql
+++ Views/View_VolumeDistributionKeys.sql
@@ -1,13 +1,14 @@
 SELECT a.PlanVersion,
-       a.SKU,
+       b.AlternativeSKU,
        a.Customer,
        IIf(ISNULL(b.SalesPlanning),'NA',b.SalesPlanning) AS SalesPlanning,
-       a.Volume,
-       c.Volume AS TotVol,
-       IIf(c.Volume=0,0,a.Volume/c.Volume) AS DistrKey
+       SUM(a.Volume) AS Volume,
+       SUM(c.Volume) AS TotVol,
+       SUM(IIf(c.Volume=0,0,a.Volume/c.Volume)) AS DistrKey
 FROM (tblReferenceVolume AS a
       INNER JOIN tblSKU AS b ON a.SKU = b.SKU)
 INNER JOIN View_ReferenceVolumeOnPlanningLevel AS c ON (IIf(ISNULL(a.Customer),'NA',a.Customer)=c.Customer)
 AND (IIf(ISNULL(b.SalesPlanning),'NA',b.SalesPlanning)=c.SalesPlanning)
-AND (a.PlanVersion = c.PlanVersion);
+AND (a.PlanVersion = c.PlanVersion)
+GROUP BY a.PlanVersion, b.AlternativeSKU, a.Customer, b.SalesPlanning
 
